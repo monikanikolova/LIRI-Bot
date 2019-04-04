e8fbd1837f2701133b074458cbe7a065bc8fc525
Monika Nikolova <Monika@Ojs-MacBook-Air.local>
2019-04-04 15:08:21 -0400

    doSomething function added. logData function added

--- liri.js
+++ liri.js
@@ -4,76 +4,99 @@ var axios = require("axios");
 var moment = require('moment');
 var Spotify = require("node-spotify-api");
 var fs = require("fs");
-
-
+var spotify = new Spotify(keys.spotify);
 var term = process.argv[2];
 var search = process.argv.slice(3).join(" ");
-switch (term) {
-    case 'concert':
-        console.log(findConcert());
-        break;
-    case 'spotify':
-        console.log(findSong())
-        break;
-    case 'movie':
-        console.log(findMovie());
-        break;
-    case 'do-what-it-says':
-    console.log(doSomething());
-    
-}
+
+function start() {
+    switch (term) {
+        case "concert-this":
+            findConcert();
+            break;
+        case "spotify-this-song":
+            findSong();
+            break;
+        case "movie-this":
+            findMovie();
+            break;
+        case "do-what-it-says":
+            doSomething();
+            break;
+        default:
+            console.log("Please input command!");
+    }
+};
 
 function findSong() {
-    var spotify = new Spotify(keys.spotify);
-
+    search = search || "The Sign Ace of Base";
     spotify
-  .search({ type: 'track', query: search })
-  .then(function(response) {
-    console.log(response.data);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+        .search({ type: "track", query: search })
+        .then(function (response) {
+            var jsonData = response.tracks.items[0]
+            var songInfo = `
+            Artist: ${jsonData.artists[0].name}
+            Song Name: ${jsonData.name}
+            Preview Link: ${jsonData.preview_url}
+            Album Name: ${jsonData.album.name}
+            `;
+            logData(songInfo);
+            console.log(songInfo);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
+
 function findConcert() {
     axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp").then(function (response) {
         var jsonData = response.data[0];
         convertedDate = moment(jsonData.datetime).format("MM/DD/YYYY");
-        console.log(`Name of the venue: ${jsonData.venue.name}
-Venue Location: ${jsonData.venue.city}
-Date of the Event: ${convertedDate}
-        `);
+        var concertInfo = `
+        Name of the venue: ${jsonData.venue.name}
+        Venue Location: ${jsonData.venue.city}
+        Date of the Event: ${convertedDate}
+        `;
+        logData(concertInfo);
+        console.log(cocertInfo);
     })
-
 }
+
 function findMovie() {
+    search = search || "Mr.Nobody";
     axios.get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
             var info = response.data;
-            console.log(`Title: ${info.Title}
-Year: ${info.Year}
-IMDB Rating: ${info.imdbRating}
-Rotten Tomatoes Rating: ${info.Ratings[1].Value}
-Country: ${info.Country}
-Language: ${info.Language}
-Plot: ${info.Plot}
-Actors ${info.Actors} `
-            );
+            var movieInfo = `
+            Title: ${info.Title}
+            Year: ${info.Year}
+            IMDB Rating: ${info.imdbRating}
+            Rotten Tomatoes Rating: ${info.Ratings[1] ? info.Ratings[1].Value : "Not available"}
+            Country: ${info.Country}
+            Language: ${info.Language}
+            Plot: ${info.Plot}
+            Actors ${info.Actors} 
+            `;
+            logData(movieInfo);
+            console.log(movieInfo);
         }
     );
-
 }
 
 function doSomething() {
-    fs.readFile("random.txt", "utf8", function(error, data) {
-
-        if (error) {
-          return console.log(error);
-        }
-      
-        console.log(data);
-        
-      
-      });
-      
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) return console.log(err);
+        var newData = data.split(",");
+        term = newData[0];
+        search = newData[1];
+        start();
+    });
 }
+
+function logData(data) {
+    fs.appendFile('log.txt', data, (err) => {
+        if (err) return console.log(err);
+        console.log('The "data to append" was appended to file!');
+    });
+}
+
+start();
